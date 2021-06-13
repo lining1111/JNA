375099a301c0c8e4f5776168a671aa4529874ce5
lining1111 <770677655@qq.com>
2021-06-13 14:18:48 +0800

    jna test

---
+++ src/main/java/AntsCalls.java
@@ -0,0 +1,27 @@
+import com.sun.jna.*;
+import com.sun.jna.Native;
+import com.sun.jna.ptr.IntByReference;
+
+
+public class AntsCalls {
+    public interface CLib extends Library {
+        CLib Instance = Native.load("sayhello", CLib.class);
+
+        void hello();
+
+        int getsum(int a, int b, int[] sum);
+    }
+
+    public static void main(String[] args) {
+        CLib.Instance.hello();
+        int a = 10;
+        int b = 3;
+
+        int[] sum = new int [3];
+        CLib.Instance.getsum(a, b, sum);
+
+
+        System.out.println(sum[1]);
+    }
+
+}
